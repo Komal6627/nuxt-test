5129236d6a035c67ce103fae22072aa96b36e638
Komal Killedar <komal.killedar@corp.ola.com>
2022-08-04 13:08:48 +0530

    change in admin form

--- pages/index.vue
+++ pages/index.vue
@@ -3,31 +3,40 @@
     <div class="text-center">
       <h1 class="text-2xl font-bold">Admin Login</h1>
       <form @submit="userLogin">
+        <label for="user">Select: </label>
+
+        <select name="user" id="user"  class="border-black border-2 m-1">
+          <option value="staf">Staf</option>
+          <option value="user">User</option>
+        </select>
+        <br>
         <label for="name">User Name:</label>
         <input
           type="text"
           id="name"
           v-model="user.name"
           placeholder="Enter the name"
-          class="border-black  border-2 m-1"
+          class="border-black border-2 m-1"
         />
-        <br>
+        <br />
         <label for="pass">Password:</label>
         <input
           type="password"
           id="pass"
           v-model="user.password"
           placeholder="Enter the password"
-          class="border-black  border-2 m-1"
+          class="border-black border-2 m-1"
         />
-        <br>
+        <br />
         <!-- <button @click="userLogin">Login</button> -->
-          <NuxtLink v-slot="{ navigate }" to="/bookSlot" custom>
-            <button @click="navigate" class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2">
-               Book slot
-            </button>
+        <NuxtLink v-slot="{ navigate }" to="/bookSlot" custom>
+          <button
+            @click="navigate"
+            class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
+          >
+            Book slot
+          </button>
         </NuxtLink>
-      
       </form>
     </div>
   </div>
@@ -48,10 +57,9 @@ export default {
       event.preventDefault();
       alert("You Logged in");
     },
-    bookSlot(event){
+    bookSlot(event) {
       alert("Your slot is booked");
-
-    }
+    },
   },
 };
 </script>
